310d74b4d83f1b9686f64216e1e4f1573a028acc
Pavel Yakovlev <28228@bk.ru>
2019-07-19 11:40:01 +0300

    Змейка в движении

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace Snake
 {
@@ -14,6 +15,7 @@ namespace Snake
             Console.SetBufferSize(80, 25);
             Console.SetWindowSize(80, 25);
            
+            //Отрисовка рамки
             HorizontalLine upline = new HorizontalLine(0, 78, 0, '+');
             HorizontalLine downline = new HorizontalLine(0, 78, 24, '+');
             VerticalLine leftline = new VerticalLine(0, 24, 0, '+');
@@ -24,9 +26,25 @@ namespace Snake
             downline.Draw();
             leftline.Draw();
 
+            //Отрисовка точек
             Point p = new Point(4, 5, '*');
             Snake snake = new Snake(p, 4, Direction.RIGHT);
             snake.Draw();
+
+            snake.Move();
+
+            Thread.Sleep(300);
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
+            Thread.Sleep(300);
+
 
             Console.ReadLine();
 
