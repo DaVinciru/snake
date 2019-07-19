628a548db09e10de9aa200129ebb6acfd0ee1c18
Pavel Yakovlev <28228@bk.ru>
2019-07-19 11:07:50 +0300

    Строим змейку

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -13,38 +13,20 @@ namespace Snake
             Console.SetWindowSize(1, 1);
             Console.SetBufferSize(80, 25);
             Console.SetWindowSize(80, 25);
-            /*
-            Point p1 = new Point(1, 3, '*');
-            Point p2 = new Point(4, 5, '#');
-            Point p3 = new Point(3, 5, '&');
-            Point p4 = new Point(5, 7, '@');
-
-
-            List<Point> pPoint = new List<Point>();
-            pPoint.Add(p1);
-            pPoint.Add(p2);
-            pPoint.Add(p3);
-            pPoint.Add(p4);
-
-           /* int x = numList[0];
-            int y = numList[1];
-            int z = numList[2];
-            
-            foreach (Point i in pPoint)
-            {
-                i.Draw();
-            }
-            
-            pPoint.RemoveAt(0);
-            */
+           
             HorizontalLine upline = new HorizontalLine(0, 78, 0, '+');
             HorizontalLine downline = new HorizontalLine(0, 78, 24, '+');
             VerticalLine leftline = new VerticalLine(0, 24, 0, '+');
             VerticalLine rightline = new VerticalLine(0, 24, 78, '+');
+
             rightline.Draw();
             upline.Draw();
             downline.Draw();
             leftline.Draw();
+
+            Point p = new Point(4, 5, '*');
+            Snake snake = new Snake(p, 4, Direction.RIGHT);
+            snake.Draw();
 
             Console.ReadLine();
 
